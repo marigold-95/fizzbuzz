cf80643b64c50cc1539d51a3f6ef443fe3e39319
marigold-95 <97016865+marigold-95@users.noreply.github.com>
2022-01-03 10:58:50 +0200

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+class FizzBuzz{
+public static void main(String args[]){
+ int num = 1;
+ 
+ while(num <= 100){
+ 
+ if(num%3==0 && num%5==0){
+ System.out.println("FizzBuzz");
+ } else if(num%5 == 0){
+ 
+     System.out.println("Buzz");
+     } else if(num%3==0)
+        {
+          System.out.println("Fizz");
+          }
+          else {
+                 System.out.println(num);
+          }
+  num++;
+ }
+}
+}
